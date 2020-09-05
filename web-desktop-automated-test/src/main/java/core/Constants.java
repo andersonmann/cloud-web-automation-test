e711cceab7263ed965c63dc92f37c341d455cf30
andersonmann <anderson.civil@hotmail.com>
2020-09-04 22:24:57 -0300

    browserstack credentials

---
+++ web-desktop-automated-test/src/main/java/core/Constants.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package core;
+
+/**
+ * @author andersonmann
+ *
+ */
+public class Constants {
+
+	public static final String USER_BS = "andersonmann3";
+	public static final String ACCESS_BS = "NtyGWz49mDrydwPCQtq6";
+	public static final String APPIUM_BS = "https://" + USER_BS + ":" + ACCESS_BS
+			+ "@hub-cloud.browserstack.com/wd/hub";
+
+}
