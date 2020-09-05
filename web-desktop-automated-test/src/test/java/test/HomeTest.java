ed3dd960e3fe7f77b5fdd320a6281cbf192b28b8
andersonmann <anderson.civil@hotmail.com>
2020-09-04 22:44:03 -0300

    HomeTest class BasePage

---
+++ web-desktop-automated-test/src/test/java/test/HomeTest.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package test;
+
+import org.testng.annotations.Test;
+
+import core.BaseTest;
+
+/**
+ * @author andersonmann
+ *
+ */
+public class HomeTest extends BaseTest {
+	
+	@Test
+	
+	public void acessaPagina() {
+		
+		
+	}
+
+}
