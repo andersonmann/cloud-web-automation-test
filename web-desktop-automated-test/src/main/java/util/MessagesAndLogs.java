47fc78b1ff7d9885dc2b5816c18084ff7bd99b4a
andersonmann <anderson.civil@hotmail.com>
2020-09-07 20:45:13 -0300

    added new messages

--- web-desktop-automated-test/src/main/java/util/MessagesAndLogs.java
+++ web-desktop-automated-test/src/main/java/util/MessagesAndLogs.java
@@ -10,13 +10,17 @@ package util;
 public class MessagesAndLogs {
 
 	public static String homeLoginUrl = "https://app.zenvia.com/home/dashboard";
-	public static String invalidRegistration = "Invalid registration, check if the email you entered is already registered.";
+	public static String registrationErrorMessage = "Invalid registration, check if the email you entered is already registered.";
 	public static String creatingAccountSuccessMessage = "account successfully created";
-	
-	public static String invalidEmail = "Invalid email address";
-	public static String emptyPassword = "Insert your password";
-	public static String loginError = "Wrong email or password.";
+
+	public static String userNameErrorMessage = "Name cannot be empty";
+	public static String emailErrorMessage = "Invalid email address";
+	public static String passwordErrorMessage = "At least 8 digits, lower case letters (a-z), upper case letters (A-Z), numbers (0-9) and special characters (!@#$%&*()/-+=_^?)";
+	public static String accountErrorLog = "correctly fill in the indicated fields";
+
+	public static String emptyPasswordErrorMessage = "Insert your password";
+	public static String loginErrorErrorMessage = "Wrong email or password.";
 	public static String empytFields = "unauthorized access";
 	public static String loginSuccess = "login successfully";
-	
+
 }
