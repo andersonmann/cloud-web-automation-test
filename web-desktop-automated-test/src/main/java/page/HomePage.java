1bc8234d0a821aec6573f6b57447aa5c6f3fbc64
andersonmann <anderson.civil@hotmail.com>
2020-09-07 16:49:42 -0300

    added javadoc

--- web-desktop-automated-test/src/main/java/page/HomePage.java
+++ web-desktop-automated-test/src/main/java/page/HomePage.java
@@ -13,22 +13,41 @@ import core.BasePage;
  */
 public class HomePage extends BasePage {
 
+	/**
+	 * this method click on button log in
+	 */
 	public void clickLogin() {
 		click(By.id("btn-signin"));
 	}
 
+	/**
+	 * this method click on button create account
+	 */
 	public void clickCreateAccount() {
 		click(By.id("btn-signup-switch"));
 	}
 
+	/**
+	 * this method click on button recovery password
+	 */
 	public void clickForgetPassword() {
 		click(By.id("btn-recovery-switch"));
 	}
 
+	/**
+	 * this method type the email of user
+	 * 
+	 * @param email
+	 */
 	public void insertEmail(String email) {
 		write(By.id("email_signin"), email);
 	}
 
+	/**
+	 * this method type the password of user
+	 * 
+	 * @param password
+	 */
 	public void insertPassword(String password) {
 		write(By.id("password_signin"), password);
 	}
