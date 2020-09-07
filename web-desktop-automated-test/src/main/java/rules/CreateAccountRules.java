099450502d78a93022eb3947cb1ab5024bb580a9
andersonmann <anderson.civil@hotmail.com>
2020-09-07 18:36:23 -0300

    created class CreateAccountRules and implemented methods

---
+++ web-desktop-automated-test/src/main/java/rules/CreateAccountRules.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package rules;
+
+import org.openqa.selenium.By;
+
+import core.BasePage;
+import page.CreateAccountPage;
+import page.HomePage;
+
+/**
+ * @author andersonmann
+ *
+ */
+public class CreateAccountRules extends BasePage {
+	HomePage home = new HomePage();
+	CreateAccountPage account = new CreateAccountPage();
+
+	/**
+	 * This method fill the fields of account creation
+	 * 
+	 * @param name
+	 * @param email
+	 * @param password
+	 */
+	public void createNewAccount() {
+		account.insertName();
+		account.insertEmail();
+		account.insertPassword("Zenvia@2020");
+		account.clickCreateAccount();
+	}
+
+	/**
+	 * This method starts the account creation flow
+	 */
+	public void createNewAccountFlow() {
+		home.clickCreateAccount();
+		createNewAccount();
+	}
+
+	public void existingUser(String email) {
+		home.clickCreateAccount();
+		account.insertName();
+		write(By.id("email_s"), email);
+		account.insertPassword("Zenvia@2020");
+		account.clickCreateAccount();
+	}
+
+	/**
+	 * @return the user's name displayed at app home
+	 */
+	public String getWelcome() {
+		return account.getValue(By.xpath("//h1[@class='title is-3']"));
+	}
+}
