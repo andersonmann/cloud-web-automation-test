3388e2b723c2e19f7032a5bca010517b58e77208
andersonmann <anderson.civil@hotmail.com>
2020-09-07 20:18:51 -0300

    created new class MessagesAndLogs

---
+++ web-desktop-automated-test/src/main/java/util/MessagesAndLogs.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package util;
+
+/**
+ * @author andersonmann
+ *
+ */
+public class MessagesAndLogs {
+
+	public static String homeLoginUrl = "https://app.zenvia.com/home/dashboard";
+	public static String invalidRegistration = "Invalid registration, check if the email you entered is already registered.";
+	public static String creatingAccountSuccessMessage = "account successfully created";
+	
+	public static String invalidEmail = "Invalid email address";
+	public static String emptyPassword = "Insert your password";
+	public static String loginError = "Wrong email or password.";
+	public static String empytFields = "unauthorized access";
+	public static String loginSuccess = "login successfully";
+	
+}
