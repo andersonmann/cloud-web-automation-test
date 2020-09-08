7ee0e898a72accc82d4f5d2eb024494c2cb02057
andersonmann <anderson.civil@hotmail.com>
2020-09-07 21:51:12 -0300

    implemented new methods

--- web-desktop-automated-test/src/main/java/page/LoginPage.java
+++ web-desktop-automated-test/src/main/java/page/LoginPage.java
@@ -3,6 +3,8 @@
  */
 package page;
 
+import org.openqa.selenium.By;
+
 import core.BasePage;
 
 /**
@@ -10,11 +12,45 @@ import core.BasePage;
  *
  */
 public class LoginPage extends BasePage {
-	HomePage home = new HomePage();
 
-	public void login(String email, String password) {
-		home.insertEmail(email);
-		home.insertPassword(password);
-		home.clickLogin();
+	/**
+	 * this method type user's email
+	 * 
+	 * @param email
+	 */
+	public void insertEmail(String email) {
+		write(By.id("email_signin"), email);
+	}
+
+	/**
+	 * this method type user's password
+	 * 
+	 * @param password
+	 */
+	public void insertPassword(String password) {
+		write(By.id("password_signin"), password);
+	}
+
+	/**
+	 * this method click on button log in
+	 */
+	public void clickButtonLogin() {
+		click(By.id("btn-signin"));
+	}
+
+	/**
+	 * This method type the email to recovery password
+	 * 
+	 * @param email
+	 */
+	public void inserEmailToRecoveryPassword(String email) {
+		write(By.id("recovery-email"), email);
+	}
+
+	/**
+	 * This method click at button recovery password
+	 */
+	public void clickButtonRecoveryPassword() {
+		click(By.id("btn-send-recovery"));
 	}
 }
