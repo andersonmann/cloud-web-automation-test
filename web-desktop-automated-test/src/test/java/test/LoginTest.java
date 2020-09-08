ae8eae9874e38d184784db0e3b5194745337add6
andersonmann <anderson.civil@hotmail.com>
2020-09-07 21:53:30 -0300

    added new data provider and implemented new test method

--- web-desktop-automated-test/src/test/java/test/LoginTest.java
+++ web-desktop-automated-test/src/test/java/test/LoginTest.java
@@ -10,7 +10,7 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import core.BaseTest;
-import page.LoginPage;
+import rules.LoginRules;
 import util.MessagesAndLogs;
 
 /**
@@ -18,36 +18,50 @@ import util.MessagesAndLogs;
  *
  */
 public class LoginTest extends BaseTest {
-	LoginPage login = new LoginPage();
+	LoginRules login = new LoginRules();
 
 	@DataProvider(name = "requiredFields")
-	public static Object[][] login0() {
+	public static Object[][] requiredFields() {
 		return new Object[][] { { "", "" } };
 	}
 
 	@DataProvider(name = "invalidEmail0")
-	public static Object[][] login1() {
+	public static Object[][] invalidEmail0() {
 		return new Object[][] { { "dasdasdasdasd", "" } };
 	}
 
 	@DataProvider(name = "invalidEmail1")
-	public static Object[][] login2() {
+	public static Object[][] invalidEmail1() {
 		return new Object[][] { { "mr.robot@", "Zenvia@2020" } };
 	}
 
 	@DataProvider(name = "invalidEmail2")
-	public static Object[][] login3() {
+	public static Object[][] invalidEmail2() {
 		return new Object[][] { { "mr.robot@gmail", "Zenvia@2020" } };
 	}
 
 	@DataProvider(name = "invalidPassword")
-	public static Object[][] login4() {
+	public static Object[][] invalidPassword() {
 		return new Object[][] { { "mr.robot@robot.com", "abcde12345" } };
 	}
 
 	@DataProvider(name = "userAndPasswordValid")
-	public static Object[][] login5() {
+	public static Object[][] userAndPasswordValid() {
 		return new Object[][] { { "mr.robot@robots.com", "Zenvia@2020" } };
+	}
+
+	@DataProvider(name = "recoveryPassword")
+	public static Object[][] recoveryPassword() {
+		return new Object[][] { { "mr.robot@robots.com" } };
+	}
+	
+	@DataProvider(name = "recoveryPasswordError1")
+	public static Object[][] recoveryPasswordError1() {
+		return new Object[][] { { "" } };
+	}
+	@DataProvider(name = "recoveryPasswordError2")
+	public static Object[][] recoveryPasswordError2() {
+		return new Object[][] { { "a@a" } };
 	}
 
 	@Test(dataProvider = "requiredFields", priority = 0)
@@ -89,7 +103,29 @@ public class LoginTest extends BaseTest {
 	@Test(dataProvider = "userAndPasswordValid", priority = 5)
 	public void loginSuccessfully(String email, String password) {
 		login.login(email, password);
-		assertEquals(login.getValue(By.xpath("//span[contains(text(),'Início')]")), "Início");
+		assertEquals(login.getValue(By.xpath("//span[contains(text(),'Início')]")), MessagesAndLogs.homeApp);
 		loging(MessagesAndLogs.loginSuccess);
 	}
+
+	@Test(dataProvider = "recoveryPassword", priority = 6)
+	public void recoveryPassword(String email) {
+		login.recoveryPassword(email);
+		assertEquals(login.getValue(By.id("success-message")), MessagesAndLogs.passwordRecoverrySuccessMessage);
+		loging(MessagesAndLogs.passwordRecoverrySuccessMessage);
+	}
+	
+	@Test(dataProvider = "recoveryPasswordError1", priority = 7)
+	public void recoveryPasswordError1(String email) {
+		login.recoveryPassword(email);
+		assertEquals(login.getValue(By.id("error-recovery-email")), MessagesAndLogs.emailErrorMessage);
+		loging(MessagesAndLogs.emailErrorMessage);
+	}
+	
+	@Test(dataProvider = "recoveryPasswordError2", priority = 8)
+	public void recoveryPasswordError2(String email) {
+		login.recoveryPassword(email);
+		assertEquals(login.getValue(By.id("error-recovery-email")), MessagesAndLogs.emailErrorMessage);
+		loging(MessagesAndLogs.emailErrorMessage);
+	}
+
 }
