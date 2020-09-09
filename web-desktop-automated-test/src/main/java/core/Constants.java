78843ec5b02c99faa73651a77a79243b9194efc2
andersonmann <anderson.civil@hotmail.com>
2020-09-09 00:10:43 -0300

    updated browserStack credentials

--- web-desktop-automated-test/src/main/java/core/Constants.java
+++ web-desktop-automated-test/src/main/java/core/Constants.java
@@ -9,10 +9,8 @@ package core;
  */
 public class Constants {
 
-//	public static final String USER_BS = "mannautomation1";
-//	public static final String ACCESS_BS = "LKPpxU2HndbYfZrVHLp8";
-	public static final String USER_BS = "andersonmann3";
-	public static final String ACCESS_BS = "NtyGWz49mDrydwPCQtq6";
+	public static final String USER_BS = "mannautomation1";
+	public static final String ACCESS_BS = "LKPpxU2HndbYfZrVHLp8";
 	public static final String APPIUM_BS = "https://" + USER_BS + ":" + ACCESS_BS
 			+ "@hub-cloud.browserstack.com/wd/hub";
 
